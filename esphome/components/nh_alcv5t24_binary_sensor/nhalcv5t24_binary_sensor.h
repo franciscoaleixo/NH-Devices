0d4ff1262914d87ab0072e8c59864b5d571e6a5e
Francisco Aleixo <francisco.aleixo16@gmail.com>
2023-12-29 00:31:46 +0000

    Test

--- esphome/components/nh_alcv5t24_binary_sensor/nhalcv5t24_binary_sensor.h
+++ esphome/components/nh_alcv5t24_binary_sensor/nhalcv5t24_binary_sensor.h
@@ -6,7 +6,12 @@
 namespace esphome {
     namespace nh_alcv5t24 {
         namespace nh_alcv5t24_binary_sensor {
-            class BinarySensor : public binary_sensor::BinarySensorInitiallyOff, public Component {};
+            class BinarySensor : public binary_sensor::BinarySensorInitiallyOff, public Component {
+                public:
+                    void dump_config() {
+                        // Do nothing
+                    }
+            };
         } // namespace nh_alcv5t24_binary_sensor
     } // namespace nh_alcv5t24
 } // namespace esphome
